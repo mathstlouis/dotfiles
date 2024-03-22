e70ed349af8619a7524dacacade452eb92949b88
Mathieu St-Louis <mathieu.st-louis@beneva.ca>
2024-03-22 13:34:32 -0400

    added aws helper function

--- aws/functions.zsh
+++ aws/functions.zsh
@@ -1,6 +1,10 @@
+function get-newest-addon {
+  check-addon $1 $2 | jq 'first'
+}
+
 function check-addon {
   aws eks describe-addon-versions --addon-name $1 \
     --query 'addons[*].addonVersions[*].addonVersion' \
     --kubernetes-version $2 \
-    --output table
+    --output json | jq 'flatten'
 }
