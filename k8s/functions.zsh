34e86fc144146a693f79e7d960fc4cab3b8d0210
Mathieu St-Louis <mathieu.st-louis@beneva.ca>
2024-03-30 08:23:54 -0400

    brewfile: added gh kustomize helm

--- k8s/functions.zsh
+++ k8s/functions.zsh
@@ -8,3 +8,7 @@ function kinspect() {
 
         kubectl run --rm -i -t mathstlouis --image=gcr.io/google-containers/toolbox --restart=Never --labels 'sidecar.istio.io/inject=false' ${nsarg} --command /bin/bash
 }
+
+function kgK() {
+        kubectl get Kustomization -n flux-system -o json | jq -r '.items[] |[.metadata.name,.spec.suspend,.status.conditions[0].lastTransitionTime,.status.conditions[0].message] | @tsv' | column -ts $'\t'
+}
