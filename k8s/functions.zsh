084f6597a09ce89a18162443700adf2a12329a5f
Mathieu St-Louis <mathieu.st-louis@beneva.ca>
2024-05-08 09:31:31 -0400

    more tools in brewfile

--- k8s/functions.zsh
+++ k8s/functions.zsh
@@ -9,6 +9,17 @@ function kinspect() {
         kubectl run --rm -i -t mathstlouis --image=gcr.io/google-containers/toolbox --restart=Never --labels 'sidecar.istio.io/inject=false' ${nsarg} --command /bin/bash
 }
 
+function kinspectb() {
+        local ns=${1}
+        local nsarg=""
+
+        if [[ ${ns} != "" ]]; then
+                nsarg="--namespace=${ns}"
+        fi
+
+        kubectl run --rm -i -t mathstlouis --image=gcr.io/corp-prod-gkeinfra/busybox:1.32 --restart=Never --labels 'sidecar.istio.io/inject=false' ${nsarg} --command /bin/sh
+}
+
 function kgK() {
         kubectl get Kustomization -n flux-system -o json | jq -r '.items[] |[.metadata.name,.spec.suspend,.status.conditions[0].lastTransitionTime,.status.conditions[0].message] | @tsv' | column -ts $'\t'
 }
